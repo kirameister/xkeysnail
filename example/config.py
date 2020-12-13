5a4f02d487e40938e0bc25b1169ad909fc194612
Akira K <kira@kirameister.net>
2020-12-13 01:54:40 +0100

    Add example config for Shingeta-Hairetsu

--- example/config.py
+++ example/config.py
@@ -3,8 +3,82 @@
 import re
 from xkeysnail.transform import *
 
-# define timeout for multipurpose_modmap
-define_timeout(1)
+# define timeout (milliseconds) for multipurpose_modmap
+define_timeout(1000)
+
+# define timeout for simyltaneous key press detection
+define_simultaneous_key_timeout(250)
+
+'''
+define_simultaneous_keymap('org.fcitx.Fcitx', re.compile('mozc'), {
+    (Key.J, Key.D): [Key.A], # あ
+    (Key.K): [Key.I], # い
+    (Key.J): [Key.U], # う
+    (Key.D, Key.SEMICOLON): [Key.E], # え
+    (Key.D, Key.L): [Key.O], # お
+
+    (Key.D): [Key.K, Key.A], # か
+    (Key.C): [Key.K, Key.I], # き
+    (Key.H): [Key.K, Key.U], # く
+    (Key.L, Key.E): [Key.K, Key.E], # け
+    (Key.I): [Key.K, Key.O], # こ
+
+    (Key.S, Key.L): [Key.S, Key.A], # さ
+    (Key.L): [Key.S, Key.I], # し
+    (Key.Z): [Key.S, Key.U], # す
+    (Key.D, Key.N): [Key.S, Key.E], # せ
+    (Key.S, Key.SEMICOLON): [Key.S, Key.O],  # そ
+
+    (Key.M): [Key.T, Key.A],
+    (Key.T): [Key.T, Key.I],
+    (Key.B): [Key.T, Key.U],
+    (Key.N): [Key.T, Key.E],
+    (Key.S): [Key.T, Key.O],
+
+    (Key.E): [Key.H, Key.A],
+    (Key.P): [Key.H, Key.I],
+    (Key.K, Key.E): [Key.H, Key.U],
+    (Key.D, Key.H): [Key.H, Key.E],
+    (Key.K, Key.A): [Key.H, Key.O],
+
+    (Key.X): [Key.M, Key.A],
+    (Key.D, Key.O): [Key.M, Key.I],
+    (Key.K, Key.V): [Key.M, Key.U],
+    (Key.L, Key.W): [Key.M, Key.E],
+    (Key.K, Key.F): [Key.M, Key.O],
+
+    (Key.S, Key.O): [Key.Y, Key.A],
+    (Key.K, Key.G): [Key.Y, Key.U],
+    (Key.D, Key.I): [Key.Y, Key.O],
+
+    (Key.S, Key.J): [Key.R, Key.A],
+    (Key.L, Key.F): [Key.R, Key.I],
+    (Key.V): [Key.R, Key.U],
+    (Key.K, Key.D): [Key.R, Key.E],
+    (Key.L, Key.V): [Key.R, Key.O],
+
+    (Key.S, Key.N): [Key.W, Key.A],
+    (Key.L, Key.A): [Key.W, Key.I],
+    (Key.F): [Key.N, Key.N],
+
+# punctuations
+
+    (Key.Q): [Key.MINUS],
+    (Key.R): [Key.COMMA],
+    (Key.DOT): [Key.DOT],
+    (Key.F, Key.V): [Key.LEFT_SHIFT, Key.KEY_1],
+    (Key.N, Key.J): [Key.LEFT_SHIFT, Key.SLASH],
+
+# functional
+
+    (Key.SEMICOLON): [Key.BACKSPACE],
+
+# dakuonn
+    (Key.O): [Key.G, Key.A],
+
+}, 'shingeta')
+'''
+
 
 # [Global modemap] Change modifier keys as in xmodmap
 define_modmap({
@@ -19,14 +93,14 @@ define_conditional_modmap(re.compile(r'Emacs'), {
 # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
 # released, and a modifier key when held down with another key. See Xcape,
 # Carabiner and caps2esc for ideas and concept.
-define_multipurpose_modmap(
+#define_multipurpose_modmap(
     # Enter is enter when pressed and released. Control when held down.
-    {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
+    #{Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
 
     # Capslock is escape when pressed and released. Control when held down.
     # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
     # To use this example, you can't remap capslock with define_modmap.
-)
+#)
 
 # [Conditional multipurpose modmap] Multipurpose modmap in certain conditions,
 # such as for a particular device.
@@ -40,6 +114,9 @@ define_conditional_multipurpose_modmap(lambda wm_class, device_name: device_name
    Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
 })
 
+#define_per_im_keymap(re.compile('mozc'),{
+#    K()
+#}, "ja-JP specific mappings")
 
 # Keybindings for Firefox/Chrome
 define_keymap(re.compile("Firefox|Google-chrome"), {
@@ -48,8 +125,12 @@ define_keymap(re.compile("Firefox|Google-chrome"), {
     K("C-M-k"): K("C-Shift-TAB"),
     # Type C-j to focus to the content
     K("C-j"): K("C-f6"),
+
+    # type forward-slash to invoke in-page search
+    #K('/'): K('^C-f'),
+
     # very naive "Edit in editor" feature (just an example)
-    K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
+    #K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
 }, "Firefox and Chrome")
 
 # Keybindings for Zeal https://github.com/zealdocs/zeal/
@@ -59,7 +140,7 @@ define_keymap(re.compile("Zeal"), {
 }, "Zeal")
 
 # Emacs-like keybindings in non-Emacs applications
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Terminator", "Terminal", "Gnome-terminal"), {
     # Cursor
     K("C-b"): with_mark(K("left")),
     K("C-f"): with_mark(K("right")),
